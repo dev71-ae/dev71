6359c240ccad82c332662ccb44fd3ce1d3b04762
Rashid J. Almheiri <r.muhairi@pm.me>
2024-11-14 23:55:12 +0400

    ziguanas -> rustaceans
    😭

--- flake.nix
+++ flake.nix
@@ -1,13 +1,27 @@
 {
-  description = "dev71-ae/dev71: A native client monorepo for Dev71 with a shared core written in Zig";
+  # This flake offers no environment whatsoever for the iOS App
+  # In fact, people working on the iOS app should refrain from loading the environment.
+  # to avoid nix overriding the SDKs & Swift breaking things.
+  description = "dev71-ae/dev71: Rust-only flake for building the core of Dev71 clients";
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
-      perSystem = {pkgs, ...}: {
+      perSystem = {
+        pkgs,
+        config,
+        inputs',
+        ...
+      }: let
+        toolchain = (inputs'.fenix.packages).fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "yMuSb5eQPO/bHv+Bcf/US8LVMbf/G/0MSfiPwBhiPpk=";
+        };
+      in {
         devShells.default = pkgs.mkShell {
           packages = builtins.attrValues {
-            inherit (pkgs) zig zls;
+            inherit toolchain;
+            inherit (config.treefmt.build.programs) alejandra rustfmt;
           };
         };
 
@@ -16,7 +30,7 @@
           flakeFormatter = true;
 
           programs.alejandra.enable = true;
-          programs.zig.enable = true;
+          programs.rustfmt.enable = true;
         };
       }; # perSystem
       imports = [inputs.treefmt.flakeModule];
@@ -27,5 +41,9 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
 
     treefmt.url = "github:numtide/treefmt-nix";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 }
