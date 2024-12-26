467072114ac88f22b9cc97d98d81c8bffa0a0086
Rashid J. Almheiri <r.muhairi@pm.me>
2024-12-26 19:24:55 +0400

    feat(core): setup unit tests

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,3 +3,14 @@
 pub fn core71_init() -> i32 {
     1
 }
+
+
+#[cfg(test)]
+mod test {
+    use crate::core71_init;
+
+    #[test]
+    fn test_init() {
+        assert_eq!(core71_init(), 1)
+    }
+}
