c37db4d09576b7b95af9e43edcc6cbda5bcf0bc6
Gaurav Gosain <itsgauravgosain@gmail.com>
2024-12-27 19:09:12 +0400

    feat: add libc as a rust dependency

--- src/lib.core71/src/lib.rs
+++ src/lib.core71/src/lib.rs
@@ -1,7 +1,12 @@
 // SAFETY: There is no other global function of the same name
 #[unsafe(no_mangle)]
-pub fn core71_init() -> i32 {
-   core::time::Duration::from_secs(5).as_millis() as i32
+pub extern "C" fn core71_init() -> i32 {
+    // print to stdout using libc's printf
+    unsafe {
+        libc::printf("[RUST libc::printf] hello world\n\0".as_ptr() as *const i8);
+    }
+    // return 5 seconds
+    core::time::Duration::from_secs(5).as_millis() as i32
 }
 
 #[cfg(test)]
