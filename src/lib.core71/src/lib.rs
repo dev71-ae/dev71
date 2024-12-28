42683f514183f43b5ddf16bae556106cd3c41841
Rashid J. Almheiri <r.muhairi@pm.me>
2024-12-28 12:38:14 +0400

    feat: Use aws-lc for crypto (broken)
    + dep: awslabs/ml-rs:mls-rs-crypto-awslc
    + dep: aws/aws-lc:libcrypto

--- src/lib.core71/src/lib.rs
+++ src/lib.core71/src/lib.rs
@@ -1,20 +1,36 @@
-#![no_main]
+#![no_std]
 
-// use mls_rs_crypto_openssl::OpensslCryptoProvider;
-// use mls_rs_crypto_awslc::AwsLcCryptoProvider;
+use mls_rs::{
+    identity::{
+        basic::{BasicCredential, BasicIdentityProvider},
+        SigningIdentity,
+    },
+    CipherSuite, CipherSuiteProvider, Client, CryptoProvider,
+};
 
-// const CIPHERSUITE: CipherSuite = CipherSuite::CURVE25519_AES128;
+extern crate alloc;
+use alloc::vec;
+
+const CIPHERSUITE: CipherSuite = CipherSuite::CURVE25519_AES128;
 
 // SAFETY: There is no other global function of the same name
 #[unsafe(no_mangle)]
 pub extern "C" fn core71_init() -> i32 {
-    // print to stdout using libc's printf
-    unsafe {
-        libc::printf("[RUST libc::printf] hello world\n\0".as_ptr() as *const i8);
-    }
-    // return 5 seconds
-    core::time::Duration::from_secs(5).as_millis() as i32
-}
+    let crypto_provider = mls_rs_crypto_awslc::AwsLcCryptoProvider::with_enabled_cipher_suites(vec![CIPHERSUITE]);
+    let cipher_suite = crypto_provider.cipher_suite_provider(CIPHERSUITE).unwrap();
+    let (secret, public) = cipher_suite.signature_key_generate().unwrap();
+
+    let basic_identity = BasicCredential::new("hello".as_bytes().to_vec());
+    let signing_identity = SigningIdentity::new(basic_identity.into_credential(), public);
+
+    _ = Client::builder()
+        .identity_provider(BasicIdentityProvider)
+        .crypto_provider(crypto_provider)
+        .signing_identity(signing_identity, secret, CIPHERSUITE)
+        .build();
+
+    0
+} 
 
 #[cfg(test)]
 mod test {
