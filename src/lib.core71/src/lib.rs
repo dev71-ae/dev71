c2da76d63f227126b96c69dd1efe0759742bd1b1
Gaurav Gosain <itsgauravgosain@gmail.com>
2024-12-29 20:24:59 +0400

    feat: initial attempt at build.sh

--- src/lib.core71/src/lib.rs
+++ src/lib.core71/src/lib.rs
@@ -1,43 +1,55 @@
 #![no_std]
 
-use mls_rs::{
-    identity::{
-        basic::{BasicCredential, BasicIdentityProvider},
-        SigningIdentity,
-    },
-    CipherSuite, CipherSuiteProvider, Client, CryptoProvider,
-};
+// NOTE: Since we are not using the standard library,
+// we need to provide our own panic handler and allocator.
+#[cfg(not(test))]
+use core::panic::PanicInfo;
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_: &PanicInfo) -> ! {
+    loop {}
+}
 
-extern crate alloc;
-use alloc::vec;
+use core::alloc::{GlobalAlloc, Layout};
+pub struct Allocator;
+unsafe impl GlobalAlloc for Allocator {
+    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
+        core::ptr::null_mut::<u8>()
+    }
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+        unreachable!(); // since we never allocate
+    }
+}
 
+#[global_allocator]
+static GLOBAL_ALLOCATOR: Allocator = Allocator;
+
+#[unsafe(no_mangle)]
+extern "C" fn rust_eh_personality() {}
+
+#[unsafe(no_mangle)]
+extern "C" fn _Unwind_Resume() {}
+
+// extern crate alloc;
+// use alloc::vec;
+use mls_rs::CipherSuite;
 const CIPHERSUITE: CipherSuite = CipherSuite::CURVE25519_AES128;
 
 // SAFETY: There is no other global function of the same name
 #[unsafe(no_mangle)]
 pub extern "C" fn core71_init() -> i32 {
-    let crypto_provider = mls_rs_crypto_awslc::AwsLcCryptoProvider::with_enabled_cipher_suites(vec![CIPHERSUITE]);
-    let cipher_suite = crypto_provider.cipher_suite_provider(CIPHERSUITE).unwrap();
-    let (secret, public) = cipher_suite.signature_key_generate().unwrap();
-
-    let basic_identity = BasicCredential::new("hello".as_bytes().to_vec());
-    let signing_identity = SigningIdentity::new(basic_identity.into_credential(), public);
-
-    _ = Client::builder()
-        .identity_provider(BasicIdentityProvider)
-        .crypto_provider(crypto_provider)
-        .signing_identity(signing_identity, secret, CIPHERSUITE)
-        .build();
-
-    0
-} 
-
-#[cfg(test)]
-mod test {
-    use crate::core71_init;
-
-    #[test]
-    fn test_init() {
-        assert_eq!(core71_init(), 1)
+    // FIX: uncomment the following line
+    // let _ = mls_rs_crypto_awslc::AwsLcCryptoProvider::with_enabled_cipher_suites(vec![CIPHERSUITE]);
+    unsafe {
+        libc::printf(c"CipherSuite: %d\n".as_ptr(), CIPHERSUITE);
     }
+
+    CipherSuite::all()
+        .enumerate()
+        .for_each(|(i, ciphersuite)| unsafe {
+            libc::printf(c"CipherSuite[%d]: %d\n".as_ptr(), i, ciphersuite);
+        });
+
+    71
 }
