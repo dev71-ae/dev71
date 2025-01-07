8a6a4fabc7677ccdede03385ccc7caebc9feaf53
Rashid J. Almheiri <r.muhairi@pm.me>
2025-01-08 01:13:36 +0400

    feat(build.zig): darwin constrain XcFrameworkStep

--- Build.zig
+++ Build.zig
@@ -7,26 +7,25 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const emit_xcframework = b.option(
-        bool,
-        "emit-xcframework",
-        "Build and install the xcframework for the iOS library.",
-    ) orelse builtin.target.isDarwin() and
-        target.result.os.tag == .ios;
-
     const ios_lib = createIOSLib(b, null, optimize);
     const ios_sim_lib = createIOSLib(b, .simulator, optimize);
 
-    const xcframework = XcFrameworkStep.create(b, .{
-        .name = "Prelude",
-        .libraries = &.{ ios_lib, ios_sim_lib },
-    });
-
-    if (emit_xcframework) {
-        b.getInstallStep().dependOn(&xcframework.install_dir.step);
-    }
-
     if (builtin.target.isDarwin()) {
+        const emit_xcframework = b.option(
+            bool,
+            "emit-xcframework",
+            "Build and install the xcframework for the iOS library.",
+        ) orelse (target.result.os.tag == .ios);
+
+        const xcframework = XcFrameworkStep.create(b, .{
+            .name = "Prelude",
+            .libraries = &.{ ios_lib, ios_sim_lib },
+        });
+
+        if (emit_xcframework) {
+            b.getInstallStep().dependOn(&xcframework.install_dir.step);
+        }
+
         const build_ios_step = b.step("ios", "Shorthand for zig build -Demit-xcframework");
         build_ios_step.dependOn(&xcframework.install_dir.step);
     }
--- Project.swift
+++ Project.swift
@@ -27,16 +27,11 @@ let project = Project(
             buildAction: .buildAction(
                 targets: [.target("Dev71")],
                 preActions: {
-                    var preActions: [ExecutionAction] = []
-
-                    // This has to be in the scheme, i've tried scripts
-                    if case let .string(zig) = Environment.zig {
-                        preActions.append(
-                            .executionAction(
-                                scriptText: "(cd $SRCROOT; \(zig) build ios)", target: "Dev71"))
-                    }
-
-                    return preActions
+                    guard case let .string(zig) = Environment.zig else { return [] }
+                    return [
+                        .executionAction(
+                            scriptText: "(cd $SRCROOT; \(zig) build ios)", target: "Dev71")
+                    ]
                 }()
             )
         )
